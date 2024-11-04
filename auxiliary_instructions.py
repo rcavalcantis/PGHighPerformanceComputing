a92803185b6852da17ac7be9dbabb2dd067852cb
Rodrigo CAVALCANTI <rodrigo.cavalcanti@kuehne-nagel.com>
2024-11-04 20:24:28 +0000

    auxiliary instructions

---
+++ auxiliary_instructions.py
@@ -0,0 +1,43 @@
+import os
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+exercise = 0
+while exercise != 99:
+    exercise = eval(input('Inform the exercise number [1,2,3,4,5,6,7,8,9,10,11,12]  99-Exit: '))
+    if exercise==1:
+        while True:
+            print('You are into first loop.')
+            option1 = input('Do you desire get out? Write YES to that. ')
+            if option1 == 'SIM':
+                break # Out of first loop
+            else:
+                while True:
+                    print('You are into second loop.')
+                    option2 = input('Do you desire get out? Write YES to that. ')
+                    if option2 == 'SIM':
+                        break # Out of second loop
+                print('You out from second loop. ')
+        print('You out from first loop. ')
+
+    if exercise==2:
+        for num in range(1,11):
+            if num == 5:
+                break
+            else:
+                print(num)
+        print('Loop end')
+
+    if exercise==3:
+        a = 0
+        for i in range(30):
+            if a % 2 == 0:
+                a += 1
+                continue
+            else:
+                if a % 5 == 0:
+                    break
+                else:
+                    a += 3
+        print(a)
